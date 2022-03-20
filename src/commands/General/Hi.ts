23e235fcb57a1cb90567c5729f089586f26fa856
Aryastark2 <101760949+Aryastark2@users.noreply.github.com>
2022-03-20 18:11:21 +0530

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -27,7 +27,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `Hello there, Elaina Here 💕. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
+				caption: `Hello there, akuma Here 💕. Use something from *${this.client.config.prefix}help* list if you want anything. \n`,
 			}
 		);
 	};
